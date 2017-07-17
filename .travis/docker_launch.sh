0b522b4718f067f1d3b7dc7ab61a43f806c475ad
Karl M. Davis <karl@justdavis.com>
2017-07-17 05:43:29 -0400

    Fixed the Docker container so SSH actually works.

--- .travis/docker_launch.sh
+++ .travis/docker_launch.sh
@@ -8,5 +8,12 @@ set -v
 
 # Build and start the CentOS 7 container, running systemd.
 docker build -t ansible_test_rcm/${TEST_CASE} ./.travis/${TEST_CASE}
-docker run --detach -p 127.0.0.1:13022:22 --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro ansible_test_rcm/${TEST_CASE}
+docker run \
+	--cap-add=SYS_ADMIN \
+	--detach \
+	-p 127.0.0.1:13022:22 \
+	--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro \
+	--tmpfs /run \
+	--tmpfs /run/lock \
+	ansible_test_rcm/${TEST_CASE}
 

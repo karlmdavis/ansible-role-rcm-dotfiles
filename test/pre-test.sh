ae6300899a4b1b0a176383e5a7a8c40e3eb27a63
Karl M. Davis <karl.davis@cms.hhs.gov>
2019-04-22 17:10:14 -0400

    Fixed bootstrap.sh URL.

--- test/pre-test.sh
+++ test/pre-test.sh
@@ -25,7 +25,7 @@ fi
 # * Leveraging the `bootstrap.sh` script from `karlmdavis/workstation-base-ansible-role`.
 #     * Supports both Linux and Mac OS X.
 #     * Basically just installs Python 3 and the tooling required for a `pipenv`.
-curl -s 'https://raw.githubusercontent.com/karlmdavis/workstation-base-ansible-role/master/bluebutton-server-app/bootstrap.sh' | bash -s
+curl -s 'https://raw.githubusercontent.com/karlmdavis/workstation-base-ansible-role/master/bootstrap.sh' | bash -s
 
 # Create a pipenv with the requested version of Ansible.
 pipenv install --three "${ANSIBLE_SPEC}"
